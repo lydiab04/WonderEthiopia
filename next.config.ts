14206563fe63a0585e8e486c37efdd753bcf4b79
Meseret1G <162950936+Meseret1G@users.noreply.github.com>
2026-05-31 22:15:32 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["mongoose"],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     qualities: [100, 75],
     remotePatterns: [
