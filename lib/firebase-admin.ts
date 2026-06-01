1ef4f4e4b4c52102f1ecc57e38324cb53961b2b9
Meseret1G <162950936+Meseret1G@users.noreply.github.com>
2026-06-01 08:21:48 +0300

    Remove default values for Firebase credentials

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,8 +4,8 @@ if (!admin.apps.length) {
   try {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "dummy_project_id",
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "dummy_email@account.com",
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
       }),
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
